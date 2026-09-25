5fb51cc14cdde96d73299a13dc5b00d3a4ff922b
Aa007 <aayush007.928@gmail.com>
2026-09-25 04:55:58 -0400

    update thumbprint for github-oidc

--- oidc/github-oidc.tf
+++ oidc/github-oidc.tf
@@ -42,7 +42,8 @@ resource "aws_iam_openid_connect_provider" "github" {
   # This thumbprint is from GitHub's documentation
   # Verify current value at: https://github.blog/changelog/2023-06-27-github-actions-update-on-oidc-integration-with-aws/
   thumbprint_list = [
-    "1b511abead59c6ce207077c0bf0e0043b1382612"
+    "6938fd4d98bab03faadb97b34396831e3780aea1",
+    "1c58a3a8518e8759bf075b76b750d4f2df264fcd"
   ]
 }
 
